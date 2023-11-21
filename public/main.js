80354ec20bfe90264458edbf2af9b85c75371601
pktherock <pktherock00@gmail.com>
2023-11-21 13:32:51 +0530

    fix bootstrap error

---
+++ public/main.js
@@ -0,0 +1,25 @@
+// Logout post request
+document
+  .getElementById("logOutBtn")
+  ?.addEventListener("click", handleLogoutBtnClick);
+
+// function to handle logout btn click
+async function handleLogoutBtnClick() {
+  const option = {
+    method: "POST",
+  };
+
+  try {
+    const response = await fetch("/api/v1/auth/logout", option);
+    if (response.ok && response.status === 200) {
+      location.reload();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const empType = document.getElementById("empType");
+if (empType) {
+  empType.value = empType?.getAttribute("value");
+}
