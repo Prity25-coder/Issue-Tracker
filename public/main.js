76a6e1fdf9c9d21b6488af47d5c6a7c69061e417
Prity25-coder <pritynishad480@gmail.com>
2023-12-14 19:17:05 +0530

    issue delete functionality done

--- public/main.js
+++ public/main.js
@@ -1,7 +1,10 @@
 // Logout post request
 // 1st method
 const LogoutLink = document.getElementById("logoutLink");
+
 const DeleteProjectButtons = document.querySelectorAll(".delete-project");
+
+const DeleteIssueBtn = document.querySelectorAll(".delete-issue");
 
 if (LogoutLink) {
   LogoutLink.addEventListener("click", handleLogoutBtnClick);
@@ -55,6 +58,43 @@ async function handleDeleteProject(projectId) {
     if (response.status === 200) {
       location.href = "/api/v1/projects/";
       // location.reload();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// issue
+if (DeleteIssueBtn.length > 0) {
+  DeleteIssueBtn.forEach((issueDeleteBtn) => {
+    // get project id from element
+    const issueId = issueDeleteBtn.getAttribute("issue-id");
+
+    issueDeleteBtn.addEventListener("click", () => handleDeleteIssue(issueId));
+  });
+}
+
+//handle delete issue
+async function handleDeleteIssue(issueId) {
+  console.log(issueId);
+  const isConfirm = confirm("Are you sure you want to delete this issue");
+
+  if (!isConfirm) return;
+
+  const options = {
+    method: "DELETE",
+  };
+
+  try {
+    const response = await fetch(`/api/v1/issues/${issueId}`, options);
+    console.log(response);
+    if (response.status === 401) {
+      alert("You are not allowed to do this operation");
+    }
+    if (response.status === 200) {
+      // location.href = "/api/v1/issues/";
+      location.reload();
+      
     }
   } catch (error) {
     console.log(error);
